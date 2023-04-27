a0f13860de9e515b709f63ead1969232db579df5
Domin <130866105+Dominion999@users.noreply.github.com>
2023-04-27 20:18:05 +0000

    modified:   components/Search.tsx

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,8 +2,7 @@ import { SearchQuery, Source } from "@/types";
 import { IconArrowRight, IconBulb, IconSearch } from "@tabler/icons-react";
 import endent from "endent";
 import { FC, KeyboardEvent, useEffect, useRef, useState } from "react";
-
-api_key = os.getenv("OPENAI_API_KEY")
+const apiKey = process.env.OPENAI_API_KEY;
 
 interface SearchProps {
   onSearch: (searchResult: SearchQuery) => void;
